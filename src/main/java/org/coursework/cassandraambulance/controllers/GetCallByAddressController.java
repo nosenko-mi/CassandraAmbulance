3dc1598351ec58a3183bdd2a47dd55c134bbc03d
nickolay <nosenkomi02@gmail.com>
2022-05-03 21:48:25 +0300

    [GetCallByAddreddController] refactored

--- src/main/java/org/coursework/cassandraambulance/controllers/GetCallByAddressController.java
+++ src/main/java/org/coursework/cassandraambulance/controllers/GetCallByAddressController.java
@@ -26,45 +26,8 @@ public class GetCallByAddressController extends Controller {
     public void GetCallByAddress(ActionEvent event) {
 
         GetSearchValues();
-        
-//        select * from call_by_date WHERE a_locality = 'Запоріжжя' AND a_thoroughfare = 'Нова' ALLOW FILTERING ;
-//        ResultSet rs;
-//        if(localityToSearch.isEmpty() && thoroughfareToSearch.isEmpty() && premiseToSearch.isEmpty() && subPremiseToSearch.isEmpty()){
-//            final String getCalls = "SELECT * FROM  " + StringResources.CALL_BY_ADDRESS + "  LIMIT 100";
-//            rs = DBConnector.getSession().execute(getCalls);
-//        } else if (thoroughfareToSearch.isEmpty() && premiseToSearch.isEmpty() && subPremiseToSearch.isEmpty()) {
-//            PreparedStatement selectAllCallsByAddress = DBConnector.getSession().prepare(
-//                    "SELECT * FROM  " + StringResources.CALL_BY_ADDRESS + "  WHERE a_locality = ?"
-//            );
-//            BoundStatement boundStatement = selectAllCallsByAddress.bind(localityToSearch);
-//            rs = DBConnector.getSession().execute(boundStatement);
-//        } else if (premiseToSearch.isEmpty() && subPremiseToSearch.isEmpty()){
-//            PreparedStatement selectAllCallsByAddress = DBConnector.getSession().prepare(
-//                    "SELECT * FROM  " + StringResources.CALL_BY_ADDRESS + "  WHERE a_locality = ? AND a_thoroughfare = '' LIMIT 100"
-//            );
-//            BoundStatement boundStatement = selectAllCallsByAddress.bind(localityToSearch, thoroughfareToSearch);
-//            rs = DBConnector.getSession().execute(boundStatement);
-//        } else if (subPremiseToSearch.isEmpty()) {
-//            PreparedStatement selectAllCallsByAddress = DBConnector.getSession().prepare(
-//                    "SELECT * FROM  " + StringResources.CALL_BY_ADDRESS + "  WHERE a_locality = ? AND a_thoroughfare = '' AND a_premise = '' LIMIT 100"
-//            );
-//            BoundStatement boundStatement = selectAllCallsByAddress.bind(localityToSearch, thoroughfareToSearch, premiseToSearch);
-//            rs = DBConnector.getSession().execute(boundStatement);
-//        } else if (!localityToSearch.isEmpty() && !thoroughfareToSearch.isEmpty() && !premiseToSearch.isEmpty() && !subPremiseToSearch.isEmpty()) {
-//            PreparedStatement selectAllCallsByAddress = DBConnector.getSession().prepare(
-//                    "SELECT * FROM  " + StringResources.CALL_BY_ADDRESS + "  WHERE a_locality = ? AND a_thoroughfare = '' AND a_premise = '' and a_sub_premise = ? LIMIT 100"
-//            );
-//            BoundStatement boundStatement = selectAllCallsByAddress.bind(localityToSearch, thoroughfareToSearch, premiseToSearch, subPremiseToSearch);
-//            rs = DBConnector.getSession().execute(boundStatement);
-//        } else {
-//            Alert alert = new Alert(Alert.AlertType.ERROR);
-//            alert.setTitle("Selection error");
-//            alert.setHeaderText("Possible selection options: Locality - > Thoroughfare -> Premise -> Sub premise");
-//            alert.showAndWait();
-//        }
 
         EmergencyCallTable.GetByAddress(dataTable, localityToSearch, thoroughfareToSearch, premiseToSearch, subPremiseToSearch);
-
 
     }
 
@@ -89,7 +52,7 @@ public class GetCallByAddressController extends Controller {
     public void initialize(){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Select information");
-        alert.setHeaderText("Selecting by Address may reduce the speed of the program");
+        alert.setHeaderText("Selecting by Full address may reduce the speed of the program");
         alert.showAndWait();
     }
 
