9ce8203599c40f2fcbec6bdb74f31ada046bf486
nickolay <nosenkomi02@gmail.com>
2022-05-04 14:38:01 +0300

    [GetUnitByIdController] refactored

--- src/main/java/org/coursework/cassandraambulance/controllers/GetUnitByIdController.java
+++ src/main/java/org/coursework/cassandraambulance/controllers/GetUnitByIdController.java
@@ -3,7 +3,7 @@ package org.coursework.cassandraambulance.controllers;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import org.coursework.cassandraambulance.Alerts;
 import org.coursework.cassandraambulance.models.Unit;
 import org.coursework.cassandraambulance.tables.UnitTable;
 
@@ -12,17 +12,20 @@ import java.util.UUID;
 public class GetUnitByIdController extends Controller {
 
     public TextField unitIdTextField;
-    public TableView unitTable;
+    public TableView<Unit> unitTable;
     private UUID unitId;
 
     public void GetUnitById(ActionEvent event) {
         try {
             unitId = UUID.fromString(unitIdTextField.getText());
+            UnitTable.GetById(unitTable, unitId) ;
+
         } catch (IllegalArgumentException e){
             unitId = null;
-            System.out.println(e);
+            Alerts.ParseError("Unit id can't be parsed");
+            System.out.println(e.getMessage());
+
         }
-        UnitTable.GetById(unitTable, unitId) ;
     }
 
 
